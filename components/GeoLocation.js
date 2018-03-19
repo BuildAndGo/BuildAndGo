abe69f355cdf3a15103c5217705c9fded4bf6403
Sarah Wooten <snwooten@gmail.com>
2018-03-19 11:22:16 -0400

    added android location permsion to AndroidManifest.xml

---
+++ components/GeoLocation.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  Text,
+  View,
+  StyleSheet,
+  PixelRatio,
+  TouchableHighlight,
+} from 'react-native';
+import {
+  ViroSceneNavigator,
+  ViroARSceneNavigator
+} from 'react-viro';
