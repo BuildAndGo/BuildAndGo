b89fdcb72723c86f30c21eff966818171015d7fe
Eunji Song <eunji.l.song@gmail.com>
2018-03-22 10:50:49 -0400

    changes

--- components/router.js
+++ components/router.js
@@ -8,16 +8,11 @@ import Profile from './Profile'
 import Racing from './2DRacing'
 import Winner from './Winner'
 import Loser from './Loser'
-<<<<<<< HEAD
 //var Searching = require('./AR/ARstart')
 import Searching from './AR/ARstart'
 import ARrender from './AR/ARrender'
 import Inventory from './Inventory';
 import CompleteCar from './CompleteCar'
-=======
-import ARrender from './ARrender'
-import PartsNearby from './PartsNearby'
->>>>>>> 5ee43717cb61f6a69071431cec216b91238b5dd9
 
 export const RootStack = StackNavigator({
  Auth: {
