8bcf29f38ea26cbab9843b9a223510c6557f1441
Eunji Song <eunji.l.song@gmail.com>
2018-03-23 15:50:36 -0400

    video in progress

--- buildandgo/components/2DRacing.js
+++ buildandgo/components/2DRacing.js
@@ -1,26 +1,24 @@
 import React from "react";
-import { View } from 'react-native'
+import { View } from "react-native";
 import { StackNavigator } from "react-navigation";
-import { ViroARScene, ViroVideo, Viro360Image } from "react-viro";
+import Video from "react-native-video";
 
 export default class Racing extends React.Component {
   render() {
     return (
       <View>
-        <ViroVideo
-          source={require("../assets/racing.mp4")}
-          onFinish={() => console.log('hi')
-            //this.props.sceneNavigator.viroAppProps.navigate("Profile")
-          }
-          loop={true}
-          paused={false}
-          position={[0, 2, -5]}
-          scale={[15, 15, 0]}
+        <Video
+          source={{
+            uri: "http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4"
+          }}
+          rate={1.0}
           volume={1.0}
+          muted={false}
+          resizeMode="cover"
+          repeat
+          style={{ width: 300, height: 300 }}
         />
       </View>
     );
   }
 }
-
-module.exports = Racing;
--- buildandgo/components/CompleteCar.js
+++ buildandgo/components/CompleteCar.js
@@ -2,39 +2,29 @@ import React from "react";
 import { View, Text, TouchableOpacity, Image } from "react-native";
 import styles from "./styles";
 import Inventory from "./Inventory";
-import { ViroARSceneNavigator } from "react-viro";
-
-var sharedProps = { apiKey: "C137FAFA-F8C2-4D21-AD2E-CC1DDE574BE3" };
-var FirstScene = require("./2DRacing");
 
 export default class CompleteCar extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      sharedProps: { apiKey: "C137FAFA-F8C2-4D21-AD2E-CC1DDE574BE3" },
-      type: "completeCar"
-    };
-
-  }
-
-
-  _comepleteCar() {
+  render() {
     return (
       <View style={styles.container}>
         <Text>You built your car!</Text>
-        <Image source={require('../assets/car.png')}/>
+        <Image source={require("../assets/car.png")} />
+
         <Inventory />
 
         <TouchableOpacity
-        style={styles.button}
-        onPress={() => this.props.navigation.navigate("Racing")}>
+          style={styles.button}
+          onPress={() => {
+            return this.props.navigation.navigate("Racing");
+          }}
+        >
           <Text>Start Racing</Text>
         </TouchableOpacity>
 
         <TouchableOpacity
           style={styles.button2}
           onPress={() => {
-            this.props.navigation.navigate("Searching");
+            return this.props.navigation.navigate("Searching");
           }}
         >
           <Text>Build a New Car</Text>
@@ -43,28 +33,12 @@ export default class CompleteCar extends React.Component {
         <TouchableOpacity
           style={styles.button3}
           onPress={() => {
-            this.props.navigation.navigate("Profile");
+            return this.props.navigation.navigate("Profile");
           }}
         >
           <Text>See My Profile</Text>
         </TouchableOpacity>
       </View>
     );
-  }
-
-  _videoOrCar(type) {
-    return () => {
-      this.setState({
-        type: type
-      });
-    };
-  }
-
-  render() {
-    if (this.state.type == "completeCar") {
-      return this._comepleteCar();
-    } else if (this.state.type == "video") {
-      return this._getVideo();
-    }
   }
 }
